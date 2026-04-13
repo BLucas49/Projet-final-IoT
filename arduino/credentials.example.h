6106471d6783674751471fe2b288e4b10c5b6c34
evan <evan.ferron53@gmail.com>
2026-04-13 14:28:20 +0200

    add gitignore

--- arduino/credentials.example.h
+++ arduino/credentials.example.h
@@ -2,8 +2,8 @@
 #define SECRETS_H
 
 // --- Identifiants LoRaWAN ---
-const char *DEVEUI = "AC1F09FFFE052698";                 // 16 chiffres hex
-const char *APPEUI = "5298298324659626";                 // 16 chiffres hex
-const char *APPKEY = "2F7D985F600F229DD41A668EE51A6672"; // 32 chiffres hex
+const char *DEVEUI = "DEVEUI"; // 16 chiffres hex
+const char *APPEUI = "APPEUI"; // 16 chiffres hex
+const char *APPKEY = "APPKEY"; // 32 chiffres hex
 
 #endif
