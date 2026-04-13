123e24713a10d8aa85d198b180cc1d7c2b7632f8
evan <evan.ferron53@gmail.com>
2026-04-13 14:18:46 +0200

    add json exports of : grafana dashboard & nodered flux | add docker compose & mosiquitto conf | add main ino script & credentials

---
+++ arduino/credentials.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// --- Identifiants LoRaWAN ---
+const char *DEVEUI = "AC1F09FFFE052698";                 // 16 chiffres hex
+const char *APPEUI = "5298298324659626";                 // 16 chiffres hex
+const char *APPKEY = "2F7D985F600F229DD41A668EE51A6672"; // 32 chiffres hex
+
+#endif
